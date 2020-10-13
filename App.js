ed23fbd1c76578cc705ab2c50db52e57710358ee
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2020-10-13 15:13:02 +0600

    style components rendered by history methods

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-      <View >
-        <AddEntry />
-      </View>
+        <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }} />
+          <History />
+        </View>
       </Provider>
     )
   }
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import {
-  FontAwesome,
-  MaterialIcons,
-  MaterialCommunityIcons
+    FontAwesome,
+    MaterialIcons,
+    MaterialCommunityIcons
 } from "@expo/vector-icons"
-import { white } from "./colors"
+import { white, red, orange, blue, lightPurp, pink } from "./colors"
 
 // utils/helpers.js
 
@@ -48,10 +48,22 @@ export function calculateDirection(heading) {
 export function timeToString(time = Date.now()) {
     const date = new Date(time);
     const todayUTC = new Date(
-      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+        Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
     );
     return todayUTC.toISOString().split("T")[0];
-  }
+}
+
+const styles = StyleSheet.create({
+    iconContainer: {
+        padding: 5,
+        borderRadius: 8,
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 20
+    },
+})
 
 export function getMetricMetaInfo(metric) {
     const info = {
@@ -63,10 +75,10 @@ export function getMetricMetaInfo(metric) {
             type: "steppers",
             getIcon() {
                 return (
-                    <View>
+                    <View style={[styles.iconContainer, { backgroundColor: red }]}>
                         <MaterialIcons
                             name='directions-run'
-                            color={'black'}
+                            color={'white'}
                             size={35}
                         />
                     </View>
@@ -81,10 +93,10 @@ export function getMetricMetaInfo(metric) {
             type: "steppers",
             getIcon() {
                 return (
-                    <View>
+                    <View style={[styles.iconContainer, { backgroundColor: orange }]}>
                         <MaterialCommunityIcons
                             name='bike'
-                            color={'black'}
+                            color={'white'}
                             size={35}
                         />
                     </View>
@@ -99,10 +111,10 @@ export function getMetricMetaInfo(metric) {
             type: "steppers",
             getIcon() {
                 return (
-                    <View>
+                    <View style={[styles.iconContainer, { backgroundColor: blue }]}>
                         <MaterialCommunityIcons
                             name='swim'
-                            color={'black'}
+                            color={'white'}
                             size={35}
                         />
                     </View>
@@ -117,10 +129,10 @@ export function getMetricMetaInfo(metric) {
             type: "slider",
             getIcon() {
                 return (
-                    <View>
+                    <View style={[styles.iconContainer, { backgroundColor: lightPurp }]}>
                         <FontAwesome
                             name='bed'
-                            color={'black'}
+                            color={'white'}
                             size={35}
                         />
                     </View>
@@ -135,10 +147,10 @@ export function getMetricMetaInfo(metric) {
             type: "slider",
             getIcon() {
                 return (
-                    <View>
+                    <View style={[styles.iconContainer, { backgroundColor: pink }]}>
                         <MaterialCommunityIcons
                             name='food'
-                            color={'black'}
+                            color={'white'}
                             size={35}
                         />
                     </View>
@@ -149,8 +161,10 @@ export function getMetricMetaInfo(metric) {
     return typeof metric === "undefined" ? info : info[metric];
 }
 
-export function getDailyReminderValue () {
-    return {
-      today: "👋 Don't forget to log your data today!"
-    }
-  }
+export function getDailyReminderValue() {
+    return [
+        {
+            today: "👋 Don't forget to log your data today!",
+        },
+    ];
+}
