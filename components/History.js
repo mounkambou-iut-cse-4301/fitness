bbd9c75c645f02a7c39b319c277c24bef2f6647c
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2020-10-14 22:02:43 +0600

    fix: this fixes undefined metric data

--- components/History.js
+++ components/History.js
@@ -53,6 +53,7 @@ class History extends Component {
     )
 
     onDayPress = (day) => {
+        console.log("day", day);
         this.setState({
             selectedDate: day.dateString,
         });
